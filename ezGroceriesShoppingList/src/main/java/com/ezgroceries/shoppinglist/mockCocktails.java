69b0214b6243ed0973f460e9bbbd629367d6fd9d
Gabriel Afilipoie <gabriel.afilipoie@kbc.be>
2024-06-20 16:41:11 +0300

    created Cocktail API

---
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/mockCocktails.java
@@ -0,0 +1,10 @@
+package com.ezgroceries.shoppinglist;
+
+public interface mockCocktails {
+    String cocktailId = "";
+    String name = "";
+    String glass = "";
+    String instructions = "";
+    String image = "";
+    String[] ingredients = new String[0];
+}
---
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/model/Cocktail.java
@@ -0,0 +1,69 @@
+package com.ezgroceries.shoppinglist.model;
+
+import java.util.ArrayList;
+
+public class Cocktail {
+    private String cocktailId;
+    private String name;
+    private String glass;
+    private String instructions;
+    private String image;
+    private ArrayList<String> ingredients;
+
+    public Cocktail(String cocktailId, String name, String glass, String instructions, String image, ArrayList<String> ingredients) {
+        this.cocktailId = cocktailId;
+        this.name = name;
+        this.glass = glass;
+        this.instructions = instructions;
+        this.image = image;
+        this.ingredients = ingredients;
+    }
+
+    public String getCocktailId() {
+        return cocktailId;
+    }
+
+    public void setCocktailId(String cocktailId) {
+        this.cocktailId = cocktailId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGlass() {
+        return glass;
+    }
+
+    public void setGlass(String glass) {
+        this.glass = glass;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
